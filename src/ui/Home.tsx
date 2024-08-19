ed18694c4b95ad741b6e8a77b60ea244b7f52ef8
Igor Stepanov <stipyk1309@gmail.com>
2024-08-19 21:37:21 +0300

    feat: app routing with react-router-dom v6

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,13 +1,17 @@
+import { Link } from 'react-router-dom'
+
 function Home() {
-  return (
-    <div>
-      <h1>
-        The best pizza.
-        <br />
-        Straight out of the oven, straight to you.
-      </h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>
+				The best pizza.
+				<br />
+				Straight out of the oven, straight to you.
+			</h1>
+
+			<Link to='/menu'>Start Ordering</Link>
+		</div>
+	)
 }
 
-export default Home;
+export default Home
