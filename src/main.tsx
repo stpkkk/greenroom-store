4231816b6bffc70e945444445220f8f6a4355ed3
Igor Stepanov <stipyk1309@gmail.com>
2024-08-21 00:18:21 +0300

    fix: main.tsx remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import '../index.css'
-import { createServer } from 'miragejs'
-import { productsData } from './data/products.ts'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
