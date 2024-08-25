96e8499dba39242d6e106ba793cc65a4ff78c7e1
Igor Stepanov <stipyk1309@gmail.com>
2024-08-25 21:55:20 +0300

    fix: error than reload page, put router to root file
    fix error: Unexpected token '<', "<!doctype "... is not valid JSON

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './ui/Home'
+import Error from './ui/Error'
+import Menu, { loader as productsLoader } from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import Order, { loader as orderLoader } from './features/order/Order'
+import CreateOrder from './features/order/CreateOrder'
+import AppLayout from './ui/AppLayout'
 import { productsData } from './data/products'
 import { Server } from 'miragejs'
 import { ordersData } from './data/orders'
-import { router } from './router'
+
+//import.meta.env.VITE_API_URL - same as process.env.REACT_APP_API_URL;
 
 new Server({
 	routes() {
-		//import.meta.env.VITE_API_URL - same as process.env.REACT_APP_API_URL;
 		this.urlPrefix = import.meta.env.VITE_API_URL
 		this.namespace = 'api'
 
@@ -25,6 +32,40 @@ new Server({
 		})
 	},
 })
+
+const router = createBrowserRouter([
+	{
+		element: <AppLayout />,
+		errorElement: <Error />,
+
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: '/menu',
+				element: <Menu />,
+				loader: productsLoader,
+				errorElement: <Error />,
+			},
+			{
+				path: '/cart',
+				element: <Cart />,
+			},
+			{
+				path: '/order/new',
+				element: <CreateOrder />,
+			},
+			{
+				path: '/order/:orderId',
+				element: <Order />,
+				loader: orderLoader,
+				errorElement: <Error />,
+			},
+		],
+	},
+])
 
 function App() {
 	return <RouterProvider router={router} />
