4e18cd4a846bff996d6a1dc1e539af173e323d44
Igor Stepanov <stipyk1309@gmail.com>
2024-09-05 11:35:09 +0000

    feat: add Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Roboto Mono, monospace',
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
+  },
+  plugins: [],
+};
